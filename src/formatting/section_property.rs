f2c0663eee4144dc06db9ced1b7a9d71b6de5fa7
John <m@DESKTOP-7TRGB04.localdomain>
2022-07-17 10:02:12 +0800

    more section property

--- src/formatting/section_property.rs
+++ src/formatting/section_property.rs
@@ -27,11 +27,11 @@ pub struct SectionProperty<'a> {
     /// Specifies the content of a run
     pub header_footer_references: Vec<HeaderFooterReference<'a>>,
     ///  Section-Wide Footnote Properties
-    //#[xml(child = "w:footnotePr")]
-    //pub footnote_property: Option<FootnoteProperty>,
+    #[xml(child = "w:footnotePr")]
+    pub footnote_property: Option<FootnoteProperty>,
     ///  Section-Wide Endnote Properties
-    //#[xml(child = "w:endnotePr")]
-    //pub endnote_property: Option<EndnoteProperty>,
+    #[xml(child = "w:endnotePr")]
+    pub endnote_property: Option<EndnoteProperty>,
     ///  Section Type
     #[xml(child = "w:type")]
     pub ty: Option<SectionTypeP>,
@@ -102,11 +102,11 @@ pub struct PreviousSectionProperty<'a> {
     pub rsid_r_default: Option<Cow<'a, str>>,
 
     ///  Section-Wide Footnote Properties
-    //#[xml(child = "w:footnotePr")]
-    //pub footnote_property: Option<FootnoteProperty>,
+    #[xml(child = "w:footnotePr")]
+    pub footnote_property: Option<FootnoteProperty>,
     ///  Section-Wide Endnote Properties
-    //#[xml(child = "w:endnotePr")]
-    //pub endnote_property: Option<EndnoteProperty>,
+    #[xml(child = "w:endnotePr")]
+    pub endnote_property: Option<EndnoteProperty>,
     ///  Section Type
     #[xml(child = "w:type")]
     pub ty: Option<SectionTypeP>,
@@ -305,6 +305,260 @@ __string_enum! {
 #[cfg_attr(test, derive(PartialEq))]
 #[xml(tag = "w:titlePg")]
 pub struct TitlePage {}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:footnotePr")]
+pub struct FootnoteProperty {
+    #[xml(child = "w:pos")]
+    pub position: Option<FootnotePosition>,
+    #[xml(child = "w:numFmt")]
+    pub num_fmt: Option<NumFmt>,
+    /// Footnote and Endnote Numbering Starting Value
+    #[xml(child = "w:numStart")]
+    pub num_start: Option<NumStart>,
+    /// Footnote and Endnote Numbering Restart Location
+    #[xml(child = "w:numRestart")]
+    pub num_restart: Option<NumRestart>,
+}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:endnotePr")]
+pub struct EndnoteProperty {
+    #[xml(child = "w:pos")]
+    pub position: Option<EndnotePosition>,
+    #[xml(child = "w:numFmt")]
+    pub num_fmt: Option<NumFmt>,
+    /// Footnote and Endnote Numbering Starting Value
+    #[xml(child = "w:numStart")]
+    pub num_start: Option<NumStart>,
+    /// Footnote and Endnote Numbering Restart Location
+    #[xml(child = "w:numRestart")]
+    pub num_restart: Option<NumRestart>,
+}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:numRestart")]
+pub struct NumRestart {
+    #[xml(attr = "w:val")]
+    pub val: NumRestartType,
+}
+
+#[derive(Debug, Default, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+pub enum NumRestartType {
+    #[default]
+    Continuous, //	Continue Numbering From Previous Section
+    EachSect, //	Restart Numbering For Each Section
+    EachPage, //	Restart Numbering On Each Page
+}
+
+__string_enum! {
+    NumRestartType {
+        Continuous = "continuous",
+        EachSect = "eachSect",
+        EachPage = "eachPage",
+    }
+}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:numStart")]
+pub struct NumStart {
+    #[xml(attr = "w:val")]
+    pub val: Option<isize>,
+}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:numFmt")]
+pub struct NumFmt {
+    #[xml(attr = "w:val")]
+    pub ty: NumFmtType,
+}
+
+#[derive(Debug, Default, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+pub enum NumFmtType {
+    #[default]
+    Decimal, //Decimal Numbers.
+    UpperRoman,                   //Uppercase Roman Numerals.
+    LowerRoman,                   //Lowercase Roman Numerals.
+    UpperLetter,                  //Uppercase Latin Alphabet.
+    LowerLetter,                  //Lowercase Latin Alphabet.
+    Ordinal,                      //Ordinal.
+    CardinalText,                 //Cardinal Text.
+    OrdinalText,                  //Ordinal Text.
+    Hex,                          //Hexadecimal Numbering.
+    Chicago,                      //Chicago Manual of Style.
+    IdeographDigital,             //Ideographs.
+    JapaneseCounting,             //Japanese Counting System.
+    Aiueo,                        //AIUEO Order Hiragana.
+    Iroha,                        //Iroha Ordered Katakana.
+    DecimalFullWidth,             //Double Byte Arabic Numerals.
+    DecimalHalfWidth,             //Single Byte Arabic Numerals.
+    JapaneseLegal,                //Japanese Legal Numbering.
+    JapaneseDigitalTenThousand,   //Japanese Digital Ten Thousand Counting System.
+    DecimalEnclosedCircle,        //Decimal Numbers Enclosed in a Circle.
+    DecimalFullWidth2,            //Double Byte Arabic Numerals Alternate.
+    AiueoFullWidth,               //Full-Width AIUEO Order Hiragana.
+    IrohaFullWidth,               //Full-Width Iroha Ordered Katakana.
+    DecimalZero,                  //Initial Zero Arabic Numerals.
+    Bullet,                       //Bullet.
+    Ganada,                       //Korean Ganada Numbering.
+    Chosung,                      //Korean Chosung Numbering.
+    DecimalEnclosedFullstop,      //Decimal Numbers Followed by a Period.
+    DecimalEnclosedParen,         //Decimal Numbers Enclosed in Parenthesis.
+    DecimalEnclosedCircleChinese, //Decimal Numbers Enclosed in a Circle.
+    IdeographEnclosedCircle,      //Ideographs Enclosed in a Circle.
+    IdeographTraditional,         //Traditional Ideograph Format.
+    IdeographZodiac,              //Zodiac Ideograph Format.
+    IdeographZodiacTraditional,   //Traditional Zodiac Ideograph Format.
+    TaiwaneseCounting,            //Taiwanese Counting System.
+    IdeographLegalTraditional,    //Traditional Legal Ideograph Format.
+    TaiwaneseCountingThousand,    //Taiwanese Counting Thousand System.
+    TaiwaneseDigital,             //Taiwanese Digital Counting System.
+    ChineseCounting,              //Chinese Counting System.
+    ChineseLegalSimplified,       //Chinese Legal Simplified Format.
+    ChineseCountingThousand,      //Chinese Counting Thousand System.
+    KoreanDigital,                //Korean Digital Counting System.
+    KoreanCounting,               //Korean Counting System.
+    KoreanLegal,                  //Korean Legal Numbering.
+    KoreanDigital2,               //Korean Digital Counting System Alternate.
+    VietnameseCounting,           //Vietnamese Numerals.
+    RussianLower,                 //Lowercase Russian Alphabet.
+    RussianUpper,                 //Uppercase Russian Alphabet.
+    None,                         //No Numbering.
+    NumberInDash,                 //Number With Dashes.
+    Hebrew1,                      //Hebrew Numerals.
+    Hebrew2,                      //Hebrew Alphabet.
+    ArabicAlpha,                  //Arabic Alphabet.
+    ArabicAbjad,                  //Arabic Abjad Numerals.
+    HindiVowels,                  //Hindi Vowels.
+    HindiConsonants,              //Hindi Consonants.
+    HindiNumbers,                 //Hindi Numbers.
+    HindiCounting,                //Hindi Counting System.
+    ThaiLetters,                  //Thai Letters.
+    ThaiNumbers,                  //Thai Numerals.
+    ThaiCounting,                 //Thai Counting System.
+}
+
+__string_enum! {
+    NumFmtType {
+        Decimal = "decimal",
+        UpperRoman = "upperRoman",
+        LowerRoman = "lowerRoman",
+        UpperLetter = "upperLetter",
+        LowerLetter = "lowerLetter",
+        Ordinal = "ordinal",
+        CardinalText = "cardinalText",
+        OrdinalText = "ordinalText",
+        Hex = "hex",
+        Chicago = "chicago",
+        IdeographDigital = "ideographDigital",
+        JapaneseCounting = "japaneseCounting",
+        Aiueo = "aiueo",
+        Iroha = "iroha",
+        DecimalFullWidth = "decimalFullWidth",
+        DecimalHalfWidth = "decimalHalfWidth",
+        JapaneseLegal = "japaneseLegal",
+        JapaneseDigitalTenThousand = "japaneseDigitalTenThousand",
+        DecimalEnclosedCircle = "decimalEnclosedCircle",
+        DecimalFullWidth2 = "decimalFullWidth2",
+        AiueoFullWidth = "aiueoFullWidth",
+        IrohaFullWidth = "irohaFullWidth",
+        DecimalZero = "decimalZero",
+        Bullet = "bullet",
+        Ganada = "ganada",
+        Chosung = "chosung",
+        DecimalEnclosedFullstop = "decimalEnclosedFullstop",
+        DecimalEnclosedParen = "decimalEnclosedParen",
+        DecimalEnclosedCircleChinese = "decimalEnclosedCircleChinese",
+        IdeographEnclosedCircle = "ideographEnclosedCircle",
+        IdeographTraditional = "ideographTraditional",
+        IdeographZodiac = "ideographZodiac",
+        IdeographZodiacTraditional = "ideographZodiacTraditional",
+        TaiwaneseCounting = "taiwaneseCounting",
+        IdeographLegalTraditional = "ideographLegalTraditional",
+        TaiwaneseCountingThousand = "taiwaneseCountingThousand",
+        TaiwaneseDigital = "taiwaneseDigital",
+        ChineseCounting = "chineseCounting",
+        ChineseLegalSimplified = "chineseLegalSimplified",
+        ChineseCountingThousand = "chineseCountingThousand",
+        KoreanDigital = "koreanDigital",
+        KoreanCounting = "koreanCounting",
+        KoreanLegal = "koreanLegal",
+        KoreanDigital2 = "koreanDigital2",
+        VietnameseCounting = "vietnameseCounting",
+        RussianLower = "russianLower",
+        RussianUpper = "russianUpper",
+        None = "none",
+        NumberInDash = "numberInDash",
+        Hebrew1 = "hebrew1",
+        Hebrew2 = "hebrew2",
+        ArabicAlpha = "arabicAlpha",
+        ArabicAbjad = "arabicAbjad",
+        HindiVowels = "hindiVowels",
+        HindiConsonants = "hindiConsonants",
+        HindiNumbers = "hindiNumbers",
+        HindiCounting = "hindiCounting",
+        ThaiLetters = "thaiLetters",
+        ThaiNumbers = "thaiNumbers",
+        ThaiCounting = "thaiCounting",
+    }
+}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:pos")]
+pub struct FootnotePosition {
+    #[xml(attr = "w:val")]
+    pub val: PositionType,
+}
+
+#[derive(Debug, Default, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+pub enum PositionType {
+    #[default]
+    PageBottom, //	Footnotes Positioned at Page Bottom
+    BeneathText, //	Footnotes Positioned Beneath Text
+    SectEnd,     //	Footnotes Positioned At End of Section
+    DocEnd,      //	Footnotes Positioned At End of Document
+}
+
+__string_enum! {
+    PositionType {
+        PageBottom = "pageBottom",
+        BeneathText = "beneathText",
+        SectEnd = "sectEnd",
+        DocEnd = "docEnd",
+    }
+}
+
+#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+#[xml(tag = "w:pos")]
+pub struct EndnotePosition {
+    #[xml(attr = "w:val")]
+    pub val: EndnotePositionType,
+}
+
+#[derive(Debug, Default, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+pub enum EndnotePositionType {
+    #[default]
+    SectEnd, //	Footnotes Positioned At End of Section
+    DocEnd, //	Footnotes Positioned At End of Document
+}
+
+__string_enum! {
+    EndnotePositionType {
+        SectEnd = "sectEnd",
+        DocEnd = "docEnd",
+    }
+}
 
 impl<'a> SectionProperty<'a> {
     //     __setter!(style_id: Option<SectionStyleId<'a>>);
