d0ec6039814d4226041dff95c259c4a03a0af4e2
John <m@DESKTOP-7TRGB04.localdomain>
2022-12-23 11:59:20 +0800

    hack for docgrid

--- src/formatting/page_grid.rs
+++ src/formatting/page_grid.rs
@@ -1,6 +1,8 @@
+use std::str::FromStr;
+
 use strong_xml::{XmlRead, XmlWrite};
 
-use crate::__string_enum;
+use crate::{__string_enum, __xml_test_suites, __setter};
 
 /// Numbering Id
 ///
@@ -21,23 +23,58 @@ pub struct PageGrid {
     pub char_space: Option<isize>,
 }
 
-#[derive(Debug, Clone)]
+impl PageGrid {
+    __setter!(ty: Option<GridType>);
+    __setter!(line_pitch: Option<isize>);
+    __setter!(char_space: Option<isize>);
+}
+
+#[derive(Debug, Clone, Default)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum GridType {
+    #[default]
     Default,       //	No Document Grid
     Lines,         //	Line Grid Only
     LinesAndChars, //	Line and Character Grid
     SnapToChars,   //	Character Grid Only
 }
 
-__string_enum! {
-    GridType {
-        Default = "default",
-        Lines = "lines",
-        LinesAndChars = "linesAndChars",
-        SnapToChars = "snapToChars",
+// hack
+impl FromStr for GridType {
+    type Err = String;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let v =match  value {
+            "0" | "default" => Self::Default,
+            "1" | "lines" => Self::Lines,
+            "2" | "linesAndChars" => Self::LinesAndChars,
+            "3" | "snapToChars" => Self::SnapToChars,
+            _ => Self::Default
+        };
+
+        Ok(v)
     }
 }
+
+impl std::fmt::Display for GridType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match  *self {
+            GridType::Default => write!(f, "default"),
+            GridType::Lines => write!(f, "lines"),
+            GridType::LinesAndChars => write!(f, "lineAndChars"),
+            GridType::SnapToChars => write!(f, "snapToChars"),
+        }
+    }
+}
+
+// __string_enum! {
+//     GridType {
+//         Default = "default",
+//         Lines = "lines",
+//         LinesAndChars = "linesAndChars",
+//         SnapToChars = "snapToChars",
+//     }
+// }
 
 // impl<T: Into<isize>> From<T> for NumberingId {
 //     fn from(val: T) -> Self {
@@ -45,8 +82,8 @@ __string_enum! {
 //     }
 // }
 
-// __xml_test_suites!(
-//     NumberingId,
-//     NumberingId::from(40isize),
-//     r#"<w:numId w:val="40"/>"#,
-// );
+__xml_test_suites!(
+    PageGrid,
+    PageGrid::default().ty(GridType::Lines),
+    r#"<w:docGrid w:type="lines"/>"#,
+);
